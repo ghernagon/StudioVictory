0dc614cfc785c0b1348c8c8174fe6a7a9749df6a
Gerardo Hernández <ghernagon@gmail.com>
2016-02-14 18:42:55 -0430

    creando proyecto y e instalando dependencias

---
+++ app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Loading dependencies
+var express = require('express');
+var path = require('path');
+
+// Body Parser
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+
+// Logger
+var logger = require('morgan');
+
+// Cookies / Session
+var cookieParser = require('cookie-parser');
+
+// Routes
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Initializing express application
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// Export application or start the server
+if(!!module.parent) {
+  module.exports = app;
+} else {
+  app.listen(3000);
+}
